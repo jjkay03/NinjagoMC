4d50718d6de09fc3e0d26328e8bc3c25cd44df3d
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-01-06 20:41:33 +0100

    Update NinjagoMC.kt

--- src/main/java/com/jjkay03/ninjagomc/NinjagoMC.kt
+++ src/main/java/com/jjkay03/ninjagomc/NinjagoMC.kt
@@ -1,13 +1,35 @@
 package com.jjkay03.ninjagomc
 
+import org.bukkit.Bukkit
+import org.bukkit.command.ConsoleCommandSender
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.logging.Logger
 
 class NinjagoMC : JavaPlugin() {
-    override fun onEnable() {
-        // Plugin startup logic
+    companion object {
+        lateinit var instance: NinjagoMC // Define a companion object to hold the instance
     }
 
+    // Plugin startup logic
+    override fun onEnable() {
+        instance = this
+        val console: ConsoleCommandSender = Bukkit.getConsoleSender()
+
+        // Plugin welcome message
+        console.sendMessage("")
+        console.sendMessage("§a┌-----------------------------┐")
+        console.sendMessage("§a|          NINJAGOMC          |")
+        console.sendMessage("§a|         IS RUNNING!         |")
+        console.sendMessage("§a└-----------------------------┘")
+        console.sendMessage("")
+
+        // Display plugin version
+        logger.info("Plugin version: ${description.version}")
+
+    }
+
+    // Plugin startup logic
     override fun onDisable() {
-        // Plugin shutdown logic
+
     }
 }
