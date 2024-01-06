f5b35373bc28858cd95360c43104e25f0d42e933
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-01-06 20:32:01 +0100

    Creating project

---
+++ src/main/java/com/jjkay03/ninjagomc/NinjagoMC.kt
@@ -0,0 +1,13 @@
+package com.jjkay03.ninjagomc
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NinjagoMC : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
