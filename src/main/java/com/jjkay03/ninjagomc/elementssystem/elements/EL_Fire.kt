681f44865e30d98caa9a4a7ffd7cd4f3d76363ce
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-01-14 20:08:53 +0100

    Update EL_Fire.kt

--- src/main/java/com/jjkay03/ninjagomc/elementssystem/elements/EL_Fire.kt
+++ src/main/java/com/jjkay03/ninjagomc/elementssystem/elements/EL_Fire.kt
@@ -3,11 +3,15 @@ package com.jjkay03.ninjagomc.elementssystem.elements
 import com.destroystokyo.paper.ParticleBuilder
 import com.jjkay03.ninjagomc.elementssystem.ElementsID
 import com.jjkay03.ninjagomc.elementssystem.ElementsUtils
+import org.bukkit.Material
 import org.bukkit.Particle
+import org.bukkit.Sound
 import org.bukkit.entity.Entity
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEntityEvent
+import org.bukkit.event.player.PlayerInteractEvent
 
 class EL_Fire : Listener {
 
@@ -18,7 +22,7 @@ class EL_Fire : Listener {
         .force(true)
         .allPlayers()
 
-    // Set entity on fire when hitting it
+    // Set entity on fire ability
     @EventHandler
     fun onInteractEntity(event: PlayerInteractEntityEvent) {
         val player = event.player
@@ -26,12 +30,50 @@ class EL_Fire : Listener {
         // Check if player has element
         if (!ElementsUtils.hasElement(player, ElementsID.FIRE)) { return }
 
+        // Play the flint and steel sound
+        player.world.playSound(player.location, Sound.ITEM_FLINTANDSTEEL_USE, 1.0f, 1.0f)
+
         // Summon fire particles around the player
         baseFireAttackParticles.location(player.location.add(0.0, 1.0, 0.0)).spawn()
 
         // Set the entity on fire
         val entity: Entity = event.rightClicked
         entity.fireTicks = 100
+    }
+
+    @EventHandler
+    fun onRightClickBlock(event: PlayerInteractEvent) {
+        val player = event.player
+
+        // Check if player has element
+        if (!ElementsUtils.hasElement(player, ElementsID.FIRE)) { return }
+
+        // Check if the action is a right-click on a block
+        if (event.action == Action.RIGHT_CLICK_BLOCK) {
+            // Check if the player's hand is empty
+            if (player.inventory.itemInMainHand.type == Material.AIR) {
+                val clickedBlock = event.clickedBlock
+                val blockFace = event.blockFace
+                // Check if the event has a valid clicked block
+                if (clickedBlock != null) {
+                    // Check if the clicked block is not an inventory block (chest, item frame...)
+                    if (!clickedBlock.type.isInteractable) {
+                        // Check if the target block is air before placing fire block
+                        val targetBlock = clickedBlock.getRelative(blockFace)
+                        if (targetBlock.type == Material.AIR) {
+                            // Play the flint and steel sound
+                            player.world.playSound(player.location, Sound.ITEM_FLINTANDSTEEL_USE, 1.0f, 1.0f)
+
+                            // Place fire particle at the block face
+                            baseFireAttackParticles.location(player.location.add(0.0, 1.0, 0.0)).count(2).spawn()
+
+                            // Place fire block
+                            targetBlock.type = Material.FIRE
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
