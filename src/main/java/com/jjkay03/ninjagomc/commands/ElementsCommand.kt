54f023f1194fb9499b20faceb56b5185180da874
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-01-14 18:42:26 +0100

    Elements command support other player

--- src/main/java/com/jjkay03/ninjagomc/commands/ElementsCommand.kt
+++ src/main/java/com/jjkay03/ninjagomc/commands/ElementsCommand.kt
@@ -4,6 +4,7 @@ package com.jjkay03.ninjagomc.commands
 import com.jjkay03.ninjagomc.NinjagoMC
 import com.jjkay03.ninjagomc.utility.PlayerData
 import com.jjkay03.ninjagomc.elementssystem.ElementsID
+import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -13,21 +14,31 @@ class ElementsCommand : CommandExecutor {
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (sender is Player) {
-            val elementsList = PlayerData.readElementsList(sender)
-
-            if (elementsList.isNotEmpty()) {
-                val playerElements = elementsList.mapNotNull { elementId ->
-                    ElementsID.values().find { it.id.equals(elementId, ignoreCase = true) }
-                }
-
-                if (playerElements.isNotEmpty()) {
-                    val elementLabels = playerElements.map { it.label }
-                    sender.sendMessage("${NinjagoMC.PREFIX} §7Your elements are: ${elementLabels.joinToString("§7, ")}")
-                } else {
-                    sender.sendMessage("${NinjagoMC.PREFIX} §cNo matching elements found!")
-                }
+            val targetPlayer: Player? = if (args.isNotEmpty()) {
+                Bukkit.getPlayer(args[0]) ?: Bukkit.getOfflinePlayer(args[0]).player
             } else {
-                sender.sendMessage("${NinjagoMC.PREFIX} §cYour have no elements!")
+                sender
+            }
+
+            targetPlayer?.let {
+                val elementsList = PlayerData.readElementsList(targetPlayer)
+
+                if (elementsList.isNotEmpty()) {
+                    val playerElements = elementsList.mapNotNull { elementId ->
+                        ElementsID.values().find { it.id.equals(elementId, ignoreCase = true) }
+                    }
+
+                    if (playerElements.isNotEmpty()) {
+                        val elementLabels = playerElements.map { it.label }
+                        sender.sendMessage("${NinjagoMC.PREFIX} §7Elements of ${targetPlayer.name}: ${elementLabels.joinToString("§7, ")}")
+                    } else {
+                        sender.sendMessage("${NinjagoMC.PREFIX} §cNo matching elements found for ${targetPlayer.name}!")
+                    }
+                } else {
+                    sender.sendMessage("${NinjagoMC.PREFIX} §c${targetPlayer.name} has no elements!")
+                }
+            } ?: run {
+                sender.sendMessage("${NinjagoMC.PREFIX} §cPlayer not found!")
             }
         } else {
             sender.sendMessage("§cOnly players can execute this command")
