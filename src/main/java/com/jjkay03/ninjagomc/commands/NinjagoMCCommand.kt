c197a96d9608de5ff505c2003d7a0351544162ac
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-01-19 22:45:00 +0100

    Finished updating /ninjago command

--- src/main/java/com/jjkay03/ninjagomc/commands/NinjagoMCCommand.kt
+++ src/main/java/com/jjkay03/ninjagomc/commands/NinjagoMCCommand.kt
@@ -7,16 +7,19 @@ import org.bukkit.command.CommandSender
 
 class NinjagoMCCommand() : CommandExecutor {
     override fun onCommand(sender: CommandSender, cmd: Command, label: String, args: Array<out String>): Boolean {
+        val modrinthLink = "https://modrinth.com/plugin/ninjagomc"
+        val githubLink = "https://github.com/jjkay03/NinjagoMC"
+
         sender.sendMessage("${NinjagoMC.PREFIX}§a§l§m---------------------------------------------")
         sender.sendMessage("${NinjagoMC.PREFIX} ")
         sender.sendMessage("${NinjagoMC.PREFIX}§2§lNinjagoMC is running!")
         sender.sendMessage("${NinjagoMC.PREFIX}§7Ninjago elements in Minecraft")
         sender.sendMessage("${NinjagoMC.PREFIX} ")
-        sender.sendMessage("${NinjagoMC.PREFIX}§7⏵ Version: §a")
+        sender.sendMessage("${NinjagoMC.PREFIX}§7⏵ Version: §a${NinjagoMC.instance.description.version}")
         sender.sendMessage("${NinjagoMC.PREFIX}§7⏵ Author: §6jjkay03")
         sender.sendMessage("${NinjagoMC.PREFIX} ")
-        sender.sendMessage("${NinjagoMC.PREFIX}§7⏵ Modrinth: §ehttps://modrinth.com/plugin/ninjagomc")
-        sender.sendMessage("${NinjagoMC.PREFIX}§7⏵ GitHub: §ehttps://github.com/jjkay03/NinjagoMC")
+        sender.sendMessage("${NinjagoMC.PREFIX}§7⏵ Modrinth: §e${modrinthLink}")
+        sender.sendMessage("${NinjagoMC.PREFIX}§7⏵ GitHub: §e${githubLink}")
         sender.sendMessage("${NinjagoMC.PREFIX} ")
         sender.sendMessage("${NinjagoMC.PREFIX}§a§l§m---------------------------------------------")
         return true
